578e6c001df64d7a04d90a63f2cca52a9c3ceb19
dileeprrishee <dileeprkvr@gmail.com>
2020-10-04 12:19:55 +0530

    cart layout updated

---
+++ app/src/main/java/com/staging/emexcart/Activities/AccountDetails.java
@@ -0,0 +1,16 @@
+package com.staging.emexcart.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.staging.emexcart.R;
+
+public class AccountDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account_details);
+    }
+}
